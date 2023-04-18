033980f008c6674ab57e2dbd72c6705e985ca4ec
JoePilarmeo <xfjpilarmeo612@gmail.com>
2023-04-18 09:28:10 +0800

    Upload files done

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <p>Created by <a href="https://github.com/JoePilarmeo">JoePilarmeo</a> from BSIT3-B1</p>
+    </div>
+</footer>
+  </body>
+</html>
